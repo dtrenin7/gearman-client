4d2cfd31cd5f4c316fdc9a36cf23789441f23ada
dtrenin <dtrenin7@gmail.com>
2017-07-07 13:06:46 +0300

    no shit here

--- src/gearman_client.cpp
+++ src/gearman_client.cpp
@@ -1,12 +1,3 @@
-/*
-# Gearman server and library
-# Copyright (C) 2012 Data Differential, http://datadifferential.com/
-# All rights reserved.
-#
-# Use and distribution licensed under the BSD license.  See
-# the COPYING file in this directory for full text.
-*/
-
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
